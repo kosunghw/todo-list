8a8dd3b96f1ed0bc3369168e0d43d27241f5dfd7
Sung <sung272012@hotmail.com>
2024-08-15 00:19:37 -0400

    hide delete project buttons and show on hover

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,6 +1,6 @@
 import { format } from "date-fns";
 import Project, { createProject } from "./Project";
-import Task from "./Task";
+import Task, { createTask } from "./Task";
 import TodoList from "./TodoList";
 
 export default class UI {
@@ -14,6 +14,13 @@ export default class UI {
   static exampleProjects() {
     const exampleProject1 = createProject("Study", "black");
     const exampleProject2 = createProject("Work", "red");
+    const exampleTask1 = createTask(
+      "LeetCode",
+      "Solve problems on LeetCode",
+      "08-24-2024",
+      "MEDIUM"
+    );
+    exampleProject1.toDoList.appendTask(exampleTask1);
     UI.projectArray.push(exampleProject1);
     UI.projectArray.push(exampleProject2);
     UI.render();
@@ -76,6 +83,7 @@ export default class UI {
 
     projectName.textContent = name;
     deleteBtn.setAttribute("id", "delete-project-btn");
+    deleteBtn.classList.add("hide");
     deleteBtn.textContent = "delete";
 
     projectItem.classList.add("project-container-item");
@@ -121,112 +129,3 @@ export default class UI {
     console.log(UI.projectArray);
   }
 }
-
-// const UI = function () {
-//   const exampleProject1 = new Project("Study", "black");
-//   const exampleProject2 = new Project("Work", "red");
-//   const projectList = [exampleProject1, exampleProject2];
-
-//   // cache DOM
-//   const addProjectBtn = document.querySelector("#add-project-btn");
-//   const projectDialog = document.querySelector("#add-project-dialog");
-//   const closeBtn = document.querySelector("#project-dialog-close");
-//   const projectForm = document.querySelector("#add-project-form");
-//   const projectContainer = document.querySelector(".project-container");
-
-//   // bind events
-//   addProjectBtn.addEventListener("click", () => {
-//     projectDialog.showModal();
-//   });
-
-//   closeBtn.addEventListener("click", () => {
-//     projectDialog.close();
-//     projectForm.reset();
-//   });
-
-//   // render();
-
-//   projectForm.addEventListener("submit", (e) => {
-//     e.preventDefault();
-//     const name = document.querySelector("#project-name").value;
-//     const color = document.querySelector("#project-color").value;
-//     const newProject = new Project(name, color);
-//     console.log(projectList);
-//     projectList.push(newProject);
-//     console.log(projectList);
-//     projectDialog.close();
-//     projectForm.reset();
-
-//     render();
-//   });
-
-//   // functions
-//   function render() {
-//     console.log("Hi, I'm in render function!");
-//     clearProjects();
-//     projectList.forEach((project) => {
-//       const name = project.name;
-//       const color = project.color;
-//       const projectItem = document.createElement("div");
-//       projectItem.classList.add("project-container-item");
-//       projectItem.innerHTML = `<span class="${color}"></span> ${name}`;
-//       projectContainer.appendChild(projectItem);
-//     });
-//   }
-
-//   function clearProjects() {
-//     projectContainer.innerHTML = `<div class="project-container-title">
-//             <div id="projects">My projects</div>
-//             <button id="add-project-btn"><span class="icon"></span></button>
-//           </div>`;
-//   }
-
-//   render();
-// };
-
-// export default class UI {
-//   static init() {
-//     this.projectList = new Array();
-//     UI.cacheDOM();
-//     UI.createEventListeners();
-//   }
-
-//   static cacheDOM() {
-//     this.addProjectBtn = document.querySelector("#add-project-btn");
-//     this.projectDialog = document.querySelector("#add-project-dialog");
-//     this.closeBtn = document.querySelector("#project-dialog-close");
-//     this.projectForm = document.querySelector("#add-project-form");
-//     this.projectContainer = document.querySelector(".project-container");
-//   }
-
-//   static createEventListeners() {
-//     // Pop up modal
-//     this.addProjectBtn.addEventListener("click", () => {
-//       this.projectDialog.showModal();
-//     });
-
-//     // Close modal
-//     this.closeBtn.addEventListener("click", () => {
-//       this.projectDialog.close();
-//       this.projectForm.reset();
-//     });
-
-//     //
-//     this.projectForm.addEventListener("submit", (e) => {
-//       e.preventDefault();
-//       const name = document.querySelector("#project-name").value;
-//       const color = document.querySelector("#project-color").value;
-//       const newProject = new Project(name, color);
-//       console.log(this.projectList);
-//       //   this.projectList.push(newProject);
-//       this.projectDialog.close();
-//       this.projectForm.reset();
-
-//       UI.render();
-//     });
-//   }
-
-//   static render() {
-//     console.log("Hello, I'm in");
-//   }
-// }
